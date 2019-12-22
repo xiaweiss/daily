d08c72abe7194a281d0b12df905a19a2824598c3
xiawei <xiaweiss@foxmail.com>
2019-12-22 22:45:14 +0800

    191222

--- leetCode/1.两数之和.js
+++ leetCode/1.两数之和.js
@@ -3,21 +3,33 @@
  *
  * [1] 两数之和
  */
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            // console.log(i, j)
-            // console.log(nums[i], nums[j])
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+    const indicies = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        if (indicies[target - num] != null) {
+            return [indicies[target - num], i]
         }
+        indicies[num] = i
     }
 };
+
+/**
+ * 数据结构：
+ * 1. 原始数组
+ * 2. 对象 indicies 保存已经遍历过的数组值
+ *
+ * 题解：
+ * 1.遍历数组
+ * 2.保存已经遍历过的值
+ * 3.检查 target - 数组当前值，在已遍历的对象中是否可以找到
+ */
 
 console.log(twoSum([3,2,4], 6))
--- leetCode/26.删除排序数组中的重复项.js
+++ leetCode/26.删除排序数组中的重复项.js
@@ -4,29 +4,45 @@
  * [26] 删除排序数组中的重复项
  */
 
-// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var removeDuplicates = function(nums) {
+//     if(nums.length ==0){
+//         return 0;
+//     }
+//     var i = 0;
+//     for(var j=1;j<nums.length;j++){
+//         if(nums[i] !== nums[j]){
+//             i++;
+//             nums[i]=nums[j];
+//         }
+//     }
+//     return i+1; // 数组长度
+// };
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if(nums.length ==0){
-        return 0;
-    }
-    var i = 0;
-    for(var j=1;j<nums.length;j++){
-        if(nums[i] !== nums[j]){
-            i++;
-            nums[i]=nums[j];
+    let len = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[len++] = nums[i]
         }
     }
-    return i+1; // 数组长度
+    return len
 };
-// @lc code=end
-// let a = [1,1,2]
-// let a = [0,0,1,1,1,2,2,3,3,4]
-
-// console.log(removeDuplicates(a))
-// console.log(a)
+/**
+ * 数据结构：
+ * 1. 原始数组
+ * 2. len 输出数组的长度，运算过程中可以认为是下标
+ *
+ * 题解：
+ * 1. 遍历原始数组，比较当前项和前一项是否一致
+ * 2. 不一致时，以 len 为依据，更新原始数组。
+ * 3. 遍历完成，输出结果。其中原始数组长度 len 的部分为已去重部分，剩下的是冗余数据
+ */
 
 

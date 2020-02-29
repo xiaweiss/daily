3fbddbdeb4a2791e211348ba80445222a162f289
xiawei <xiaweiss@foxmail.com>
2020-02-29 23:27:21 +0800

    2020-02-29

---
+++ vue-demo-2/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    {{message}} {{message + message}}
+    <div :id="message"></div>
+    <todo-list>
+        <todo-item v-for="item in list" :key="item.title" :title="item.title" :del="item.del" @delete="handleDelete">
+            <!-- <span slot="pre" slot-scope="{value}">pre {{value}}</span> -->
+            <template v-slot:pre="{value}">
+                <span>pre {{value}}</span>
+            </template>
+            <!-- <template v-slot:suf>
+                <span >suf</span>
+            </template> -->
+        </todo-item>
+    </todo-list>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+// import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+        message: 'Hello, World',
+        list: [
+            {
+                title: 'class1',
+                del: false
+            },
+            {
+                title: 'class2',
+                del: true
+            }
+        ]
+    }
+  },
+  methods: {
+      handleDelete(val) {
+          console.log('handleDelete', val)
+      }
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ vue-demo-2/src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+      <slot name="pre" :value="value"></slot>
+      <span class="red" v-if="!del">{{title}}</span>
+      <span v-else style="text-decoration: line-through">{{title}}</span>
+      <slot name="suf">😄</slot>
+      <button v-show="!del" @click="handelClick(title)">del</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+      title: String,
+      del: {
+          type: Boolean,
+          default: false
+      },
+  },
+  data () {
+      return {
+          value: Math.random()
+      }
+  },
+  methods: {
+      handelClick (val) {
+          // this.delete(val)
+          this.$emit('delete', val)
+      }
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>

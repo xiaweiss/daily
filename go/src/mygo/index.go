355a8c00e8ecb49915177d1f9e21a951f2a59816
xiawei <xiaweiss@foxmail.com>
2020-05-25 00:07:51 +0800

    2020-05-25

--- go/src/mygo/index.go
+++ go/src/mygo/index.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
-	str := "abc"
-	fmt.Print(str)
+func main () {
+	var a, b = getMe("front-end:", 18)
+	fmt.Println(a) // front-end:xiawei
+	fmt.Println(b) // 18
+
+}
+
+func getMe (prefix string, age int) (string, int) {
+	name := "xiawei"
+	return prefix + name, age
 }

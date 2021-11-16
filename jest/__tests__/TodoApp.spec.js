eef0af9e303b4c32c499eb47c71f7563bc653951
Xiawei <xiaweiss@foxmail.com>
2021-11-16 15:52:59 +0800

    2021-11-16 vue3-jest

---
+++ jest/__tests__/TodoApp.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import TodoApp from '../TodoApp.vue'
+
+test('renders a todo', () => {
+  const wrapper = mount(TodoApp)
+
+  const todo = wrapper.get('[data-test="todo"]')
+
+  expect(todo.text()).toBe('Learn Vue.js 3')
+})
---
+++ jest/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  rootDir: __dirname,
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/examples/$1",
+    "^src/(.*)$": "<rootDir>/src/$1"
+  },
+  testMatch: [
+    '<rootDir>/__tests__/**/*.spec.ts',
+    '<rootDir>/__tests__/**/*.spec.js'
+  ],
+  // 默认 transform
+  transform: {
+    "\.[jt]sx?$": "babel-jest",
+    "\.vue$": "vue3-jest",
+  },
+  // testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  testPathIgnorePatterns: ['/node_modules/']
+}

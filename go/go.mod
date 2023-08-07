4a9dec887fbcecd752e3d3f416d7caae7794bb34
Xiawei <xiaweiss@foxmail.com>
2023-08-07 22:35:25 +0800

    2023-08-07

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module example/index
+
+go 1.20
--- go/index.go
+++ go/index.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main () {
-	fmt.Print("abc")
+func main() {
+	var a, b = getMe("zhangsan")
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+func getMe(prefix string) (string, int) {
+	name := prefix + "lisi"
+	age := 19
+	return name, age
 }

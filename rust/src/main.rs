4e0c7daf9283c629092b4cb07faedb73a9c3c567
Xiawei <xiaweiss@foxmail.com>
2024-04-09 22:38:24 +0800

    2024-04-09

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    let myname = "Rust";
-    println!("Hello, {}!", myname);
+    let myage:u8 = 255;
+
+    println!("我的年龄是:{}", myage);
+    println!("u8的最大值是: {}，最小值是{}", u8::max_value(), u8::min_value());
 }

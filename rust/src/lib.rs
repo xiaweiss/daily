457ec586dc001a31012b8ee8bbbb571e4b759dd9
Xiawei <xiaweiss@foxmail.com>
2024-04-10 23:34:23 +0800

    2024-04-10

---
+++ rust/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod helper {
+    pub fn show_me () {
+        let my_name = "张三";
+        let my_age = 18;
+        println!("我的名字是：{}，我的年龄是：{}", my_name, my_age);
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
-fn main() {
-    let myage:u8 = 255;
+#![allow(special_module_name)]
+mod lib;
 
-    println!("我的年龄是:{}", myage);
-    println!("u8的最大值是: {}，最小值是{}", u8::max_value(), u8::min_value());
+fn main() {
+    lib::helper::show_me();
 }
+

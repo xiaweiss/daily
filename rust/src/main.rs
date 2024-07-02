0b71f70e845788d9aa43a00ba6855c6b08349048
Xiawei <xiaweiss@foxmail.com>
2024-07-03 00:11:38 +0800

    2024-07-03

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,20 @@
-// fn get_user(uid: i32) {
-//   if uid == 101 {
-//     println!("user is {}", "foo");
-//   } else if uid == 102 {
-//     println!("user is {}", "bar");
-//   } else {
-//     println!("user is {}", "unknown")
-//   }
-// }
-
-fn get_user (uid: i32) {
-  match uid {
-    101 => println!("user is {}", "foo"),
-    102 => println!("user is {}", "bar"),
-    _ => println!("user is {}", "unknown")
-  }
+fn get_user(uid: i32) -> &'static str {
+  let ret = if uid == 101 {
+    "foo"
+  } else if uid == 102 {
+    "bar"
+  } else {
+    "unknown"
+  };
+  ret
 }
 
 fn main() {
-  let name = "张三";
+  let a = 1;
+  let b = if a == 1 { 5 } else { 10 };
+  println!("{}", b);
 
   let uid = 101;
-  get_user(uid);
+  let name = get_user(uid);
+  println!("{}", name);
 }

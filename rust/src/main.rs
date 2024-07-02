d41dcdae9a29ede096757049e43302db28f87cac
Xiawei <xiaweiss@foxmail.com>
2024-07-02 23:34:41 +0800

    2024-07-02

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,24 @@
-#![allow(special_module_name)]
-mod lib;
+// fn get_user(uid: i32) {
+//   if uid == 101 {
+//     println!("user is {}", "foo");
+//   } else if uid == 102 {
+//     println!("user is {}", "bar");
+//   } else {
+//     println!("user is {}", "unknown")
+//   }
+// }
 
-fn main () {
-    lib::helper::show_me();
+fn get_user (uid: i32) {
+  match uid {
+    101 => println!("user is {}", "foo"),
+    102 => println!("user is {}", "bar"),
+    _ => println!("user is {}", "unknown")
+  }
 }
 
+fn main() {
+  let name = "张三";
 
+  let uid = 101;
+  get_user(uid);
+}

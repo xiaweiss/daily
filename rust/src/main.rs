d73e04f519d1a435bc5a26374963901180050b06
Xiawei <xiaweiss@foxmail.com>
2024-07-02 23:05:16 +0800

    2024-07-01

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,8 @@
-// mod foo;
+#![allow(special_module_name)]
+mod lib;
 
 fn main () {
-    // foo::helper::show_me();
-    let result: &'static str = condiction(-111);
-    println!("The number is {}", result);
+    lib::helper::show_me();
 }
 
-fn condiction (num: i32) -> &'static str {
-    if num > 0 {
-        return "positive";
-    } else if num < 0 {
-        return "negative";
-    } else {
-        return "zero";
-    }
-}
 

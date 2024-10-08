1ce97e3ea8865e99346cf276356ab6f5c906dddf
Xiawei <xiaweiss@foxmail.com>
2024-10-08 22:11:46 +0800

    2024-10-08

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,33 @@
-fn get_user(uid: i32) -> &'static str {
-  let ret = if uid == 101 {
-    "foo"
-  } else if uid == 102 {
-    "bar"
-  } else {
-    "unknown"
-  };
-  ret
+// 打印每个字符、字符串长度
+// fn filter (text: &str) {
+//   for c in text.chars() {
+//     println!("{}", c)
+//   }
+//   println!("length:{}", text.len())
+// }
+
+// 判断用户名长度，4-10个字符是合法的
+fn check_username(username: &str) {
+  match username.len() {
+    0..=3 => println!("too short"),
+    4..=10 => println!("ok"),
+    _ => println!("too long"),
+  }
+}
+
+// 打印 1-10 的数字
+fn show () {
+  for i in 1..11 {
+    println!("{}", i)
+  }
 }
 
 fn main() {
-  let a = 1;
-  let b = if a == 1 { 5 } else { 10 };
-  println!("{}", b);
+  // let text = "Hello";
+  // filter(text)
 
-  let uid = 101;
-  let name = get_user(uid);
-  println!("{}", name);
+  let name = "123";
+  check_username(name);
+
+  show();
 }

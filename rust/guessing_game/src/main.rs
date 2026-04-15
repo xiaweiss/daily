e7eebbf11b97d7df262e1d028c33ebce5eae484c
Xiawei <xiaweiss@foxmail.com>
2026-04-15 22:59:43 +0800

    2026-04-15

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,21 +1,37 @@
-use std::io;
 use rand::prelude::*;
+use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    let mut rng = rand::rng();
-    let secret_number = rng.random_range(1..=100);
+    let secret_number = rand::rng().random_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+      println!("Please input your guess.");
 
-    let mut guess = String::new();
+      let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+      io::stdin()
+          .read_line(&mut guess)
+          .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+      println!("You guessed: {guess}");
+
+      let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+          };
+
+      match guess.cmp(&secret_number) {
+          Ordering::Less => println!("Too small!"),
+          Ordering::Greater => println!("Too big!"),
+          Ordering::Equal => {
+            println!("You win!");
+            break;
+          }
+      }
+    }
 }

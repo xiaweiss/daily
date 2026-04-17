d64b1007c2a26173d1a85f8f0e69b6e25a6beaa5
Xiawei <xiaweiss@foxmail.com>
2026-04-17 23:07:17 +0800

    2026-04-17

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+      let x = x * 2;
+      println!("The value of x in the inner scope is: {x}");
+    }
     println!("The value of x is: {x}");
 }

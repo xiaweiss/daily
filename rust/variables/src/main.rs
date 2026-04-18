9eb453624468dc32671b9d578d04a542c75e07e2
Xiawei <xiaweiss@foxmail.com>
2026-04-18 23:57:08 +0800

    2026-04-18

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -8,4 +8,8 @@ fn main() {
       println!("The value of x in the inner scope is: {x}");
     }
     println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The number of spaces is: {spaces}");
 }
